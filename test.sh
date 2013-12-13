4a721aefeede848501d35d9502a2a2e5c3b96d7b
Tony Montana <junggeorge@yandex.ru>
2013-12-13 23:38:43 +0400

    Fixed another error in test.sh

--- test.sh
+++ test.sh
@@ -5,9 +5,9 @@ cmake -G "Unix Makefiles" ../
 echo --------------------------------------------------------------------------
 make
 echo --------------------------------------------------------------------------
-task2 -d ../data/train_labels.txt -m model.txt --train
+./task2 -d ../data/train_labels.txt -m model.txt --train
 echo --------------------------------------------------------------------------
-task2 -d ../data/test_labels.txt -m model.txt -l predictions.txt --predict
+./task2 -d ../data/test_labels.txt -m model.txt -l predictions.txt --predict
 echo --------------------------------------------------------------------------
 echo
-task2_test -g ../data/test_labels.txt -p predictions.txt
+./task2_test -g ../data/test_labels.txt -p predictions.txt
